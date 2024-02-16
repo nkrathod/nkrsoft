a4786fd6676624330f71c511216e11ce2bab612d
Nitesh Rathod <niteshrathod15205@gmail.com>
2024-02-16 13:52:44 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 style={{color: '#bf1919'}}>NKR<span style={{color: '#27278a'}}>SOFT</span></h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in progress
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        </div>
       </header>
     </div>
   );
